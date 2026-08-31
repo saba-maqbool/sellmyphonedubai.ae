6876cc115e0084f3f0d9bbd93b888363b7c5af57
Saba Maqbool <162045621+SABA-CH17@users.noreply.github.com>
2026-08-31 10:23:35 +0500

    no-index apple page

--- Includes/header.php
+++ Includes/header.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <?php if (basename($_SERVER['PHP_SELF']) == 'apple-page.php'): ?>
+    <meta name="robots" content="noindex, nofollow">
+<?php endif; ?>
 
     <?php require_once(__DIR__ . "/get-site-settings.php"); ?>
 
